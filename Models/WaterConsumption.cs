f760d4979a875bafe3fd908116c0b45acedad077
kanuthuru shashank <shashankkanuthuru23@gmail.com>
2026-01-06 19:36:44 +0530

    #add Models/WaterManagement

---
+++ Models/WaterConsumption.cs
@@ -0,0 +1,30 @@
+﻿namespace SHSOS.Models
+{
+    public class WaterConsumption
+    {
+        public int ConsumptionID { get; set; }
+
+        public int DepartmentID { get; set; }
+
+        public DateTime ConsumptionDate { get; set; }
+
+        public decimal ReadingStart { get; set; }
+
+        public decimal ReadingEnd { get; set; }
+
+        public decimal UnitsConsumedLiters { get; set; }
+
+        public bool PeakHourFlag { get; set; }
+
+        public bool LeakageDetected { get; set; }
+
+        public string UsageCategory { get; set; }
+
+        public string WeatherCondition { get; set; }
+
+        public string Remarks { get; set; }
+
+        public DateTime RecordedAt { get; set; }
+
+    }
+}
